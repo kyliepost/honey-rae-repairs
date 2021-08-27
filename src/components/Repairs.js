f3a9874055e9d6c30235b73449c9372abc6ad195
kyliepost <kylie.postlethwaite00@gmail.com>
2021-08-27 11:38:21 -0500

    customer list renders

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"
 
 export const Repairs = () => {
-  return(
-  <h1> Honey Rae's Repair Shop </h1>
-  )
-  
+    const [customers, assignCustomers] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/customers")
+                .then(res => res.json())
+                .then((customers) => {
+                    assignCustomers(customers)
+                 }
+                )
+        },
+        []
+    )
+
+    return (
+        <>
+        <h1>Honey Rae's Repair Shop</h1>
+
+        {
+            customers.map(
+                (customerObject) => { 
+                    return <h2>{customerObject.name}</h2>
+                }
+            )
+        }
+        </>
+    )
 }

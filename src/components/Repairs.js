62ad4b7f15ce87fa39d308226ff1ee5f4e8fe2e4
kyliepost <kylie.postlethwaite00@gmail.com>
2021-09-03 09:19:37 -0500

    added my header back to the page

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -14,6 +14,7 @@ export const Repairs = () => (
           return (
             <>
               <NavBar />
+              <h1>Honey Rae's Repairs </h1>
               <ApplicationViews />
             </>
           );
--- src/components/serviceTickets/TicketForm.js
+++ src/components/serviceTickets/TicketForm.js
@@ -1,13 +1,38 @@
 import React, { useState } from "react"
+import { useHistory } from "react-router-dom";
 
 export const TicketForm = () => {
+
     const [ticket, updateTicket] = useState({
         description: "",
         emergency: false
     });
 
-    const saveTicket = (event) => {
-        event.preventDefault()
+    const history = useHistory()
+
+    const saveTicket = (evt) => {
+        evt.preventDefault()
+        
+        const newTicket = {
+            description: ticket.description,
+            emergency: ticket.emergency,
+            customerId: parseInt(localStorage.getItem("honey_customer")),
+            employeeId: 1,
+            dateCompleted: ""
+        }
+        const fetchOption = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newTicket)
+        }
+
+        return fetch("http://localhost:8088/serviceTickets", fetchOption)
+        
+        .then(() => {
+            history.push("/tickets")
+        })
     }
 
     return (
